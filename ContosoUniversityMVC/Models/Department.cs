5657ec3c672d4fe3a75c1f9ebf6fc6c0d8cdcdd7
Mark Matteus Murru <mamurr@ttu.ee>
2020-02-16 18:36:05 +0200

    Add project files.

---
+++ ContosoUniversityMVC/Models/Department.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ContosoUniversityMVC.Models
+{
+    public class Department
+    {
+        public int DepartmentID { get; set; }
+
+        [StringLength(50, MinimumLength = 3)]
+        public string Name { get; set; }
+
+        [DataType(DataType.Currency)]
+        [Column(TypeName = "money")]
+        public decimal Budget { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Start Date")]
+        public DateTime StartDate { get; set; }
+
+        public int? InstructorID { get; set; } //Therefore the InstructorID property is included as the foreign key to the Instructor entity, and a question mark is added after the int type designation to mark the property as nullable.
+
+        public Instructor Administrator { get; set; } //A department may or may not have an administrator, and an administrator is always an instructor
+                                                      // The navigation property is named Administrator but holds an Instructor entity:
+        public ICollection<Course> Courses { get; set; }
+    }
+}
---
+++ ContosoUniversityMVC/Models/Enrollment.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace ContosoUniversityMVC.Models
+{
+    public enum Grade
+    {
+        A, B, C, D, F
+    }
+
+    public class Enrollment
+    {
+        public int EnrollmentID { get; set; }
+        public int CourseID { get; set; } //An enrollment record is for a single course, so there's a CourseID foreign key property and a Course navigation property:
+        public int StudentID { get; set; }
+        [DisplayFormat(NullDisplayText = "No grade")]
+        public Grade? Grade { get; set; }
+
+        public Course Course { get; set; }
+        public Student Student { get; set; }
+    }
+}
